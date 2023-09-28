eb3c5db7a550ae9b3f4e4425e7012f8a6a658f78
gawsalya <rj_gawsalya@hotmail.co.uk>
2023-09-28 18:07:59 +0100

    app route to return titles

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 '''Script connecting API and database'''
+from os import environ
+
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 from flask import Flask, jsonify
+import pandas as pd
 from psycopg2 import connect, Error
 from psycopg2.extensions import connection
 from psycopg2.extras import RealDictCursor
 
 
-main = Flask(__name__)
+app = Flask(__name__)
+load_dotenv()
+config = environ
 
 
 def get_db_connection(config) -> connection:
@@ -20,3 +27,17 @@ def get_db_connection(config) -> connection:
             cursor_factory=RealDictCursor)
     except (Error, ValueError) as err:
         return f"Error connecting to database. {err}"
+
+
+@app.route("/", methods=["GET"])
+def database():
+    '''return database values'''
+    conn = get_db_connection(config)
+    with conn.cursor() as cur:
+        cur.execute("""SELECT title FROM game;""")
+        titles = cur.fetchall()
+        return jsonify(titles)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5050)
